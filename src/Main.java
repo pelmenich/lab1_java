84c4a5df6b7c8ccc2f358a07f510651d90a24fd0
tsalyona <olenatsarevaaa@gmail.com>
2024-04-12 22:58:20 +0300

    lab done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        int threadCount = 10;
+        int threadCount = 5;
         BreakThread breakThread = new BreakThread(threadCount);
         ThreadController threadController = new ThreadController(threadCount, breakThread);
 
